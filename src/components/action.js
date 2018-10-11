4cf77c5a26db50fab3933ee8f4e1cd9fdac3da75
joshh450 <joshh450@yahoo.com>
2018-10-11 10:22:25 -0600

    add remove course actions

--- src/components/action.js
+++ src/components/action.js
@@ -3,20 +3,35 @@ import Icon from './icon';
 
 class Action extends Component {
 
-    hanldeAction = function() {
-        this.props.onClick()
+    constructor(props) {
+        super(props)
 
-        // document.getElementById('action').classList.add('')
+        this.status = false;
+    }
+
+    handleAction = function() {
+        this.props.onClick()
+        if (!this.status) {
+            document.getElementById(this.id).classList.add('action-remove');
+        } else {
+            document.getElementById(this.id).classList.remove('action-remove');
+        }
+        this.status = !this.status;
     }.bind(this);
 
     render() {
+        this.id = `action ${this.props.id}`
         return (
-            <a id='action' onClick={() => this.props.onClick()} className={`${this.props.className} action`}>
-                {/* { Icon('fas fa-plus-circle') } */}
-                {/* { Icon('fas fa-times-circle') } */}
+            <a
+                id={this.id}
+                onClick={() => this.handleAction()}
+                className={`${this.props.className} action`}>
+
             </a>
         )
     }
+}
+
     //     return (
     //         <button onClick={() => this.props.onClick()} className={`${this.props.className} action`}>
     //             { Icon('fas fa-plus-circle') }
@@ -25,5 +40,4 @@ class Action extends Component {
     //     )
     // }
 
-}
 export default Action;
